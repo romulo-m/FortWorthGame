186afabb704c30c7588a27617c8a98335bffadff
Rômulo Miranda <romulo.mgaudencio@gmail.com>
2022-04-21 20:45:08 -0300

    finalizei a estilização de algumas paginas e o roteiro

---
+++ js/usable.js
@@ -0,0 +1,31 @@
+function infoGame() {
+    location.assign("/html/info.html")
+}
+
+function charSelect() {
+    location.assign("../html/character.html")
+}
+
+function backIndex () {
+    location.assign('../index.html')
+}
+
+
+function respostaCerta(certa, errada, gameOver, msgCerta, msgErrada, gOverMsg, proxFase, resultado) {
+    while(true){
+    let opcao = prompt("Qual a sua alternativa?")
+    if (opcao === certa) {
+        alert(msgCerta);
+        location.href = proxFase;
+        break;
+    } else if (opcao === errada) {
+    alert(msgErrada);
+    } else if (opcao === gameOver) {
+        alert(gOverMsg);
+        location.href = resultado;
+        break;    
+    } else {
+        alert ("Digite uma opção válida!!!")
+    }
+}
+}   
